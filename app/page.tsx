85225e751643c18d4a99e4f1760653e711d21ebf
riaars <riaratnasari@outlook.com>
2025-03-13 23:36:45 +0100

    improve layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { validWords } from "./data/swedish_words";
+import { validWords } from "./data/swedish_words_million";
 
 const getRandomLetters = (num: number) => {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÅÖ";
@@ -29,7 +29,7 @@ export default function Home() {
     if (validWords.includes(lowerWord)) {
       setScore(score + lowerWord.length);
       setMessage("✅ Valid Word!");
-      setSubmittedWords([...submittedWords, lowerWord]);
+      setSubmittedWords([...submittedWords, lowerWord.toUpperCase()]);
       setLetters(getRandomLetters(12));
       setFirstLetter(getRandomLetters(1));
     } else {
@@ -51,13 +51,40 @@ export default function Home() {
   };
 
   return (
-    <div className="p-4 text-center flex flex-col items-center m-10">
-      <h1 className="text-2xl font-bold">Svenska WordFlow Game</h1>
-      <p className="text-xl my-4">Score: {score}</p>
+    <div className="text-center flex flex-row justify-center h-screen">
+      <div className="flex flex-col items-center justify-center p-4 w-96 bg-gray-100">
+        <div className="fixed top-0 mt-4">
+          <h1 className="text-2xl font-bold">Svenska WordFlow Game</h1>
+          <p className="text-xl my-4">Score: {score}</p>
+        </div>
+        <div className="flex flex-col gap-2 overflow-y-auto">
+          {submittedWords.map((word, index) => (
+            <div className="flex flex-row gap-2 p-2 text-lg font-bold bg-scroll">
+              {word.split("").map((char: string) => (
+                <div
+                  key={index}
+                  className="p-4 border rounded bg-gray-200 center"
+                  style={{ maxWidth: "60px" }}
+                >
+                  {char}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
 
-      {/* <div className="grid grid-cols-4 gap-2 text-lg font-bold mt-5">
-        {submittedWords.map((word, index) =>
-          word.map((char: string) => (
+        <div
+          className="grid grid-cols-8 gap-2 text-lg font-bold mt-5"
+          style={{ maxWidth: "400px" }}
+        >
+          <div
+            className="p-2 border rounded bg-gray-200 center"
+            style={{ maxWidth: "60px" }}
+          >
+            {firstLetter}
+          </div>
+
+          {word.map((char: string, index) => (
             <div
               key={index}
               className="p-2 border rounded bg-gray-200 center"
@@ -65,79 +92,64 @@ export default function Home() {
             >
               {char}
             </div>
-          ))
-        )}
-      </div> */}
+          ))}
+        </div>
+        <p className="mt-2">{message}</p>
 
-      <div
-        className="grid grid-cols-8 gap-2 text-lg font-bold mt-5"
-        style={{ maxWidth: "400px" }}
-      >
-        <div
-          className="p-2 border rounded bg-gray-200 center"
-          style={{ maxWidth: "60px" }}
-        >
-          {firstLetter}
+        <div className="flex flex-row gap -1">
+          {word.length > 0 && (
+            <button
+              onClick={handleReset}
+              className="ml-2 p-2  text-blue-500 mt-2 mb-2 border solid rounded"
+            >
+              Clear
+            </button>
+          )}
         </div>
 
-        {word.map((char: string, index) => (
-          <div
-            key={index}
-            className="p-2 border rounded bg-gray-200 center"
-            style={{ maxWidth: "60px" }}
-          >
-            {char}
+        <div className="flex flex-col justify-baseline absolute bottom-0">
+          <div className="flex flex-row justify-between">
+            <button
+              onClick={handleRefresh}
+              className="ml-2 p-2  text-blue-500 mt-2 mb-2 border solid rounded"
+            >
+              Refresh
+            </button>
+
+            <button
+              onClick={handleSubmit}
+              className="ml-2 p-2 bg-blue-500 text-white mt-2 mb-2 border rounded"
+            >
+              Submit
+            </button>
           </div>
-        ))}
-      </div>
-      <div className="flex flex-row gap -1">
-        <button
-          onClick={handleSubmit}
-          className="ml-2 p-2 bg-blue-500 text-white mt-2 mb-2 border rounded"
-        >
-          Submit
-        </button>
-
-        <button
-          onClick={handleReset}
-          className="ml-2 p-2  text-blue-500 mt-2 mb-2 border solid rounded"
-        >
-          Clear
-        </button>
-      </div>
-
-      <button
-        onClick={handleRefresh}
-        className="ml-2 p-2  text-blue-500 mt-2 mb-2 border solid rounded"
-      >
-        Refresh
-      </button>
-      <div className="grid grid-cols-6 gap-2 text-lg font-bold center mb-2 pt-5">
-        {letters.map((char: string, index) => (
-          <div
-            key={index}
-            className={`p-4 border rounded ${
-              selectedIndices.includes(index)
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200"
-            }`}
-            onClick={() => {
-              //remove that char if the index is already selected
-              if (selectedIndices.includes(index)) {
-                setSelectedIndices(selectedIndices.filter((i) => i != index));
-                setWord(word.filter((item) => item !== char));
-              } else {
-                setWord([...word, char]);
-                setSelectedIndices([...selectedIndices, index]);
-              }
-            }}
-          >
-            {char}
+          <div className="grid grid-cols-6 gap-2 text-lg font-bold center mb-2 pt-5">
+            {letters.map((char: string, index) => (
+              <div
+                key={index}
+                className={`p-4 border rounded cursor-pointer ${
+                  selectedIndices.includes(index)
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-200"
+                }`}
+                onClick={() => {
+                  if (selectedIndices.includes(index)) {
+                    setSelectedIndices(
+                      selectedIndices.filter((i) => i != index)
+                    );
+                    setWord(word.filter((item) => item !== char));
+                  } else {
+                    setWord([...word, char]);
+                    setSelectedIndices([...selectedIndices, index]);
+                  }
+                }}
+              >
+                {char}
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-
-      <p className="mt-2">{message}</p>
     </div>
   );
 }
